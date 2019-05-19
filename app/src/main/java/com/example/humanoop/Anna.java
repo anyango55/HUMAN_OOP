bbb195e369bcff58a3d00f78bb8fc602052396d0
anyango55 <anyangoc55@gmail.com>
2019-05-19 22:16:51 +0300

    "Human OOP"

---
+++ app/src/main/java/com/example/humanoop/Anna.java
@@ -0,0 +1,23 @@
+package com.example.humanoop;
+
+
+import android.util.Log;
+
+public class Anna extends Human{
+    public Anna(String name, int age, int weight) {
+        super(name, age, weight);
+    }
+
+    private int height;
+
+    public  Anna(String name, int age, int weight, int height) {
+        super(name, age, weight);
+        this.height = height;
+    }
+
+
+
+}
+    public int weight (int increment) {
+        weight = weight + increment;
+        return weight;
---
+++ app/src/main/java/com/example/humanoop/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.humanoop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Human John = new Human("John",3,75);
+        Human Mary = new Human("Mary",29,65);
+        Human Susan = new Human("Susan",35,80);
+        Human Antoine = new Human("Antonine",8,60);
+
+
+        Log.d("Human","John name" + John.getName());
+        Log.d("Human","Mary name" + Mary.getName());
+        Log.d("Human","Susan name" + Susan.getName());
+        Anna anna = new Anna("Anna",37,50);
+        anna.eat();
+        anna.birthday(4);
+
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
